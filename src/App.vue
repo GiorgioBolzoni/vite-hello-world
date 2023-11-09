9bedaac1e097e17770e4d01905deef2b26411881
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-11-09 11:41:13 +0100

    pulito scaffolding e inserito titolo

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<!-- html -->
+<template>
+  <header>
+    <h1>{{title}}</h1>
+  </header>
+  <main>
+    <!-- sono main -->
+  </main>
+  <footer>
+    <!-- sono footer -->
+  </footer>
+</template>
+
+
+<!-- logiche -->
+<script>
+  export default {
+    data(){
+      return{
+        title: 'La mia prima app con Vite!'
+      }
+    },
+    methods:{
+
+    },
+  }
+</script>
+
+
+<!-- stile -->
+<style>
+  
+</style>
